dc587a8ddb14deede31c4e816d908c7f2fad7f0c
Toni Uimonen <tauimonen@gmail.com>
2022-08-22 12:55:05 +0300

    Change global variables to json

--- game-logic.js
+++ game-logic.js
@@ -1,9 +1,12 @@
-let playerScore = 0;
-let computerScore = 0;
-let gameInfoText = "Start game by pressing your choice";
-let gameOver = false;
 const buttons = document.querySelectorAll('.choice-button');
 const newGame = document.querySelector('.new-game');
+
+const variables = {
+    playerScore: 0,
+    computerScore: 0,
+    gameInfoText: "Start game by pressing your choice",
+    gameOver: false
+};
 
 function getComputerChoice() {
     const rndInt = Math.floor(Math.random() * 3) + 1;
@@ -33,53 +36,52 @@ function enableButtons() {
 function setScores() {
     let pScore = document.getElementById('playerScore');
     let cScore = document.getElementById('computerScore');
-    pScore.innerHTML = playerScore;
-    cScore.innerHTML = computerScore;
+    pScore.innerHTML = variables.playerScore;
+    cScore.innerHTML = variables.computerScore;
 }
 
 function setGameInfo() {
     let gameInfo = document.querySelector('.game-info');
-    gameInfo.innerHTML = gameInfoText;
+    gameInfo.innerHTML = variables.gameInfoText;
 }
 
 function playRound(playerSelection, computerSelection) {
     let winner = "";
     if (playerSelection === computerSelection) {
-        gameInfoText = "Tie!";
+        variables.gameInfoText = "Tie!";
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
                 (playerSelection === "paper" && computerSelection === "rock") ||
                 (playerSelection === "scissors" && computerSelection === "paper")) {
-        gameInfoText = "Player won!";
-        playerScore += 1;
+                    variables.gameInfoText = "Player won!";
+                    variables.playerScore += 1;
     } else if ((playerSelection === "rock" && computerSelection === "paper") ||
                 (playerSelection === "paper" && computerSelection === "scissors") ||
                 (playerSelection === "scissors" && computerSelection === "rock")) {
-        gameInfoText = "Computer won!";            
-        computerScore += 1;
+                    variables.gameInfoText = "Computer won!";            
+                    variables.computerScore += 1;
     }
    
     setScores();
     setGameInfo();
 
-    if (playerScore === 5 || computerScore === 5) {
-        if (playerScore === 5) {winner = "Player";}
-        else if (computerScore === 5) {winner = "Computer";}
-
-        gameInfoText = winner + " won the game!";
+    if (variables.playerScore === 5 || variables.computerScore === 5) {
+        if (variables.playerScore === 5) {winner = "Player";}
+        else if (variables.computerScore === 5) {winner = "Computer";}
+        variables.gameInfoText = winner + " won the game!";
         setGameInfo();
-        gameOver = true;
+        variables.gameOver = true;
         showOrHideNewGameButton();
         disableButtons();
-        gameOver = true;
+        variables.gameOver = true;
     } 
 }
 
 function setNewGame() {
-    gameOver = false;
+    variables.gameOver = false;
     showOrHideNewGameButton();
-    playerScore = 0;
-    computerScore = 0;
-    gameInfoText = "Start game by pressing your choice";
+    variables.playerScore = 0;
+    variables.computerScore = 0;
+    variables.gameInfoText = "Start game by pressing your choice";
     setScores();
     setGameInfo();
     enableButtons();
@@ -87,7 +89,7 @@ function setNewGame() {
 
 function showOrHideNewGameButton() {
     let x = document.querySelector('.new-game');
-    if (gameOver) {
+    if (variables.gameOver) {
       x.style.display = "block";
     } else {
       x.style.display = "none";
