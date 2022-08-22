95555a08b5857bb9ea7b053012d79f0dd83dd8e7
Toni Uimonen <tauimonen@gmail.com>
2022-08-22 12:34:18 +0300

    Make new game button and logic

--- game-logic.js
+++ game-logic.js
@@ -1,5 +1,9 @@
 let playerScore = 0;
 let computerScore = 0;
+let gameInfoText = "Start game by pressing your choice";
+let gameOver = false;
+const buttons = document.querySelectorAll('.choice-button');
+const newGame = document.querySelector('.new-game');
 
 function getComputerChoice() {
     const rndInt = Math.floor(Math.random() * 3) + 1;
@@ -16,7 +20,13 @@ function getComputerChoice() {
 
 function disableButtons() {
     buttons.forEach(button => {
-        button.disabled = true
+        button.disabled = true;
+    })
+}
+
+function enableButtons() {
+    buttons.forEach(button => {
+        button.disabled = false;
     })
 }
 
@@ -27,43 +37,77 @@ function setScores() {
     cScore.innerHTML = computerScore;
 }
 
+function setGameInfo() {
+    let gameInfo = document.querySelector('.game-info');
+    gameInfo.innerHTML = gameInfoText;
+}
+
 function playRound(playerSelection, computerSelection) {
     let winner = "";
     if (playerSelection === computerSelection) {
-        console.log("Tie!");
+        gameInfoText = "Tie!";
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
                 (playerSelection === "paper" && computerSelection === "rock") ||
                 (playerSelection === "scissors" && computerSelection === "paper")) {
-        console.log("Player won!");
-        playerScore++;
+        gameInfoText = "Player won!";
+        playerScore += 1;
     } else if ((playerSelection === "rock" && computerSelection === "paper") ||
                 (playerSelection === "paper" && computerSelection === "scissors") ||
                 (playerSelection === "scissors" && computerSelection === "rock")) {
-        console.log("Computer won!");            
-        computerScore++;
+        gameInfoText = "Computer won!";            
+        computerScore += 1;
     }
-
-    console.log("player: " + playerScore + ", computer: " + computerScore);
+   
     setScores();
+    setGameInfo();
 
     if (playerScore === 5 || computerScore === 5) {
         if (playerScore === 5) {winner = "Player";}
         else if (computerScore === 5) {winner = "Computer";}
 
-        console.log(winner + " won the game!")
+        gameInfoText = winner + " won the game!";
+        setGameInfo();
+        gameOver = true;
+        showOrHideNewGameButton();
         disableButtons();
+        gameOver = true;
     } 
 }
 
-const buttons = document.querySelectorAll('button')
-console.log("Start game by pressing button")
-setScores();
+function setNewGame() {
+    gameOver = false;
+    showOrHideNewGameButton();
+    playerScore = 0;
+    computerScore = 0;
+    gameInfoText = "Start game by pressing your choice";
+    setScores();
+    setGameInfo();
+    enableButtons();
+}
+
+function showOrHideNewGameButton() {
+    let x = document.querySelector('.new-game');
+    if (gameOver) {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
 
 buttons.forEach(button =>{
     button.addEventListener('click', function(){
         playRound(button.id, getComputerChoice());
     })
 })
+
+newGame.addEventListener('click', function() {
+    setNewGame();
+});
+
+
+setNewGame();
+
 
 
 
